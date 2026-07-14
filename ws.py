9751abb12dbf5d9ac7f930fb6543c02edd177e51
Student <Student@Train11-3-PC182.control.ad.sit.kmutt.ac.th>
2026-07-14 16:27:45 +0700

    write program in classroom

---
+++ ws.py
@@ -0,0 +1,10 @@
+quantity = int(input("จำนวนปืนที่รับมาขาย : "))
+cost_price = int(input("ต้นทุนของปืนที่รับมา : "))
+sell_price = int(input("ราคาที่จะนำไปขายต่อ : "))
+team_members = int(input("จำนวนลูกน้องในทีมที่ไปทำงาน : "))
+
+total_price = quantity * cost_price
+
+
+print("ราคาต้นทุน", total_price)
+
